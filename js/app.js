bcc9b309f595e4822baee2809475fd568b77f845
catperez81 <cat@healthsherpa.com>
2015-10-11 19:09:17 -0700

    add feedback function

--- js/app.js
+++ js/app.js
@@ -13,17 +13,18 @@ $(document).ready(function(){
   	});
 
 // Start a new game on clicking of "new game" button
-// Generate secret number between 1 and 100 
 function newGame () {
 	$(".new").click(function(){
+		// Generate secret number between 1 and 100 
 		return Math.floor((Math.random() * 100) + 1);
-		// console.log("Math.floor((Math.random() * 100) + 1");
 	});
+	// User should get feddback in each guess - choose your own ranges
+	$("#guessbutton").click(function(){
+		$('#feedback').hide();
+		$('#feedback').show('<h2>good guess!</h2>');
+	})
 }
 	
-
-// User should get feddback in each guess - choose your own ranges
-// Feedback should appear in div#feedback
 // Game should track how many guesses the user has made should appear in span#count
 // Supply user with list of numbers they guessed so far
 // Ensure users provide valid inputs between 1 - 100
