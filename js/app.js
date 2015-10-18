bd223057c9d53b7a804aa0f4a441ea6741ef657c
catperez81 <cat@healthsherpa.com>
2015-10-17 19:24:06 -0700

    clear field on new game

--- js/app.js
+++ js/app.js
@@ -13,35 +13,46 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+	// Generate secret number between 1 and 100
 	$('.new').click(function(){	
 		randomNumber = newGame();
 		console.log(randomNumber);
+	// clear field on new game
+		$('input[type="text"]').val('');
 	});
 
 	// Start a new game on clicking of "new game" button
 	function newGame () {
-		
-	// Generate secret number between 1 and 100
-	// $('<input type="text" class = "text'+ (++i) +'" onclick="this.value=\'\'" value="Enter Choice #' + i + '"/>');
-	
 		return Math.floor((Math.random() * 100) + 1);
+	}
+	
+	// Game should track how many guesses the user has made should appear in span#count
+	$('.guessButton').click(function() {
+    var counter = $('span#count').val();
+    counter++;
+    $('.guessButton').val(counter);
 	});
 
-// clear field on new game
-	$('.new').click(function(){
-	$('#userGuess').remove();	
-	});
+
+}); 
+
+	// $('<input type="text" class = "text'+ (++i) +'" onclick="this.value=\'\'" value="Enter Choice #' + i + '"/>');
+	// });
+
+
+	// $('.new').click(function(){
+	// $('#userGuess').remove();	
+	// });
 
 	// User should get feedback in each guess - choose your own ranges
-	$("#guessbutton").click(function(){
-	$('#feedback').hide('Make your Guess');
-	$('#feedback').append('<h2>good guess!</h2>');
-}
+// 	$("#guessbutton").click(function(){
+// 	$('#feedback').hide('Make your Guess');
+// 	$('#feedback').append('<h2>good guess!</h2>');
+// }
 	
-// Game should track how many guesses the user has made should appear in span#count
+
 // Supply user with list of numbers they guessed so far
 // Ensure users provide valid inputs between 1 - 100
 
-});
 
 
