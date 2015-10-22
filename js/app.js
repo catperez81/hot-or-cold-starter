7f469806a54abc0bd7279667fe7d5f44ec7eb366
catperez81 <cat@healthsherpa.com>
2015-10-21 18:37:17 -0700

    almost there

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	var randomNumber = 0;
+	var randomNumber = newGame();
 	var counter = 0;
 
 	/*--- Display information modal box ---*/
@@ -25,25 +25,27 @@ $(document).ready(function(){
 
 	// Start a new game on clicking of "new game"  
 	function newGame () {
-		return Math.floor((Math.random() * 100) + 1);
+		var localRandom = Math.floor((Math.random() * 100) + 1);
+		console.log(localRandom);
+		return localRandom;
 	}
+
 	// Supply user with list of numbers they guessed so far
 	$('#guessButton').click(function(event) {
 		event.preventDefault();
 		var userGuess = $('#userGuess').val();
-		alert(userGuess);
+		// alert(userGuess);
 		$('input[type="text"]').val('');
 
 	// Ensure users provide valid inputs between 1 - 100
-	var userGuess = $('#userGuess').val(''); 
 		if (userGuess > 100) {
 		alert("Please guess a number between 1 and 100!");
 	}
 	// Game should track how many guesses the user has made should appear in span#count
-    var counter = $('#count').val();
     	counter++;
-    	alert(counter);
-    $('#count').append('counter');
+    	// alert(counter);
+    	$('#count').text(counter);
+    	$('#guessList').append('<li>' + userGuess + '</li>');
 
 });
 	
@@ -51,17 +53,18 @@ $(document).ready(function(){
 }); 
 
 	// User should get feedback in each guess - choose your own ranges
-<<<<<<< HEAD
+// <<<<<<< HEAD
 	// $("#guessbutton").click(function(){
 	// $('#feedback').hide('Make your Guess');
 	// $('#feedback').append('<h2>good guess!</h2>');
-}
-=======
-// 	$("#guessbutton").click(function(){
-// 	$('#feedback').hide('Make your Guess');
-// 	$('#feedback').append('<h2>good guess!</h2>');
 // }
->>>>>>> master
+// =======
+// Game should track how many guesses the user has made should appear in span#count
+	// $("#guessbutton").click(function(){
+	// $('#feedback').hide('Make your Guess');
+	// $('#feedback').append('<h2>good guess!</h2>');
+// }
+// >>>>>>> master
 	
 
 
